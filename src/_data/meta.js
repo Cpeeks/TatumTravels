c073356f30d5d68cdfd74e33ce4d5a6cbf8b043d
Cpeeks <pikavitzki@gmail.com>
2023-04-03 12:06:26 -0400

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,13 +1,13 @@
 module.exports = {
 	siteURL: process.env.URL || 'http://localhost:8080',
-	siteName: "Shane Robinson's 11ty | TailwindCSS | Alpine.js Template",
+	siteName: "Tatum Travels",
 	siteDescription:
-		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
+		'a safe and adventurous travel group for women',
 	siteImage: '/assets/images/site/default.png',
 	lang: 'en',
 	locale: 'en_us',
-	authorName: 'Shane Robinson',
-	authorURL: 'shane-robinson',
+	authorName: 'Sarah Tatum',
+	authorURL: '',
 	authorEmail: 'shane@shanerobinson.com',
 	twitterSite: '@shanerobinson',
 	twitterCreator: '@shanerobinson',
